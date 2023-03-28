a61ff9cadb728dae352e581a960e84c6d730d863
Dmytro2V <op2vin@gmail.com>
2023-03-28 05:22:36 +0300

    done with most bonuses

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Message from "./components/Message";
 import PictureDisplay from "./components/PictureDisplay";
 
 function App() {
-  const [size, setSize] = useState('s');
+  const [size, setSize] = useState('m');
   const [featherCount, setFeatherCount] = useState(0);
   const [featherColors, setFeatherColors] = useState([]);
   const [isRed, setIsRed] = useState(false);
@@ -11,6 +11,41 @@ function App() {
   const [isBrown, setIsBrown] = useState(false);
   const [isLightBrown, setIsLightBrown] = useState(false);
   const [isYellow, setIsYellow] = useState(false);
+  const [sizeClass, setSizeClass]=useState('')  
+  useEffect(() => {
+    let c;    
+    switch (size) {
+      case 's':
+        c = 'small';
+        break;
+      case 'm':
+        c = 'medium';
+        break;
+      case 'l':
+        c = 'large';
+        break;
+      case 'xl':
+        c = 'xlarge';
+        break;
+    }
+    setSizeClass(c)
+    console.log('PictureDisplay - size ', size, c);
+  }, [size])
+
+
+
+  useEffect(() => {
+    //console.log('r:',isRed,'/o',isOrange,'/b',isBrown, '/lb',isLightBrown,'/y',isYellow);
+    const colors = [];
+    if (isRed) colors.push('red');
+    if (isOrange) colors.push('orange');
+    if (isBrown) colors.push('brown');
+    if (isLightBrown) colors.push('light-brown');
+    if (isYellow) colors.push('yellow');
+    setFeatherColors(colors)      
+  
+  }, [isRed,isOrange,isBrown,isLightBrown,isYellow])
+    
 
   return (
     <>
@@ -20,10 +55,10 @@ function App() {
       {/* User controls */}
       <div className="button-controls">
         Size:
-        <button onClick={() => setSize('s')}>Small</button>
-        <button onClick={() => setSize('m')}>Medium</button>
-        <button onClick={() => setSize('l')}>Large</button>
-        <button onClick={() => setSize('xl')}>X-Large</button>
+        <button onClick={(e) => {setSize('s');  e.currentTarget.disabled = true;}}>Small</button>
+        <button onClick={(e) => {setSize('m');  e.currentTarget.disabled = true;}}>Medium</button>
+        <button onClick={(e) => {setSize('l');  e.currentTarget.disabled = true;}}>Large</button>
+        <button onClick={(e) => {setSize('xl');  e.currentTarget.disabled = true;}}>X-Large</button>
       </div>
       <div className="button-controls">
         Feather Count:
@@ -62,11 +97,11 @@ function App() {
       {/* Generated display based on user selections above */}
       <h3 className="button-controls">Enjoy your turkey</h3>
       <PictureDisplay
-        size={size}
+        sizeClass={sizeClass}
         featherCount={featherCount}
         featherColors={featherColors}
       />
-      <Message size={size} />
+      <Message sizeClass={sizeClass} featherCount={featherCount} />
     </>
   );
 }
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,26 @@
-function Message({ size }) {
-  console.log('Message', size);
+import { useEffect, useState } from "react";
 
-  return (
-    <div className="message medium">
-      (Oh my! Your bird is naked!)
+function Message({sizeClass, featherCount }) {
+  const [message, setMessage] = useState('Oh my! Your bird is naked!')
+  useEffect(()=>{
+
+    console.log('Message, sizeClass: ', sizeClass);
+  },[sizeClass])
+
+  useEffect(() =>{
+    if (featherCount <= 0)
+        setMessage('Oh my! Your bird is naked!');
+    else if (featherCount >= 10) {
+        setMessage('Full turkey!');
+    } else {
+        setMessage('Coming along...');
+    }
+}, [featherCount])
+  
+
+  return (    
+    <div className={`message ${sizeClass}`}>
+      {message}
     </div>
   );
 };
